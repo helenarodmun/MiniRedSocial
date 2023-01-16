5695f3a85aa00c869a43f28a39ad667bbf08620d
helenarodmun <helenarodmun@gmail.com>
2023-01-16 19:02:06 -0100

    Vista index, rutas CommunityLinkController

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('community', [App\Http\Controllers\CommunityLinkController::class, 'index']);
+
+Route::post('community', [App\Http\Controllers\CommunityLinkController::class, 'store']);
+
 require __DIR__.'/auth.php';
