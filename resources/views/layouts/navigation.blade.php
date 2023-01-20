9fb58f225bca4b1a46dc9c0d29cf682f47f36b81
helenarodmun <helenarodmun@gmail.com>
2023-01-20 20:50:32 +0000

    Rectificado

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,3 @@
-@auth
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -99,4 +98,3 @@
         </div>
     </div>
 </nav>
-@endauth
