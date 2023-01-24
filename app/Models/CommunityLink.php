b34906f165257f8e10618e111f8c779b304c4311
helenarodmun <helenarodmun@gmail.com>
2023-01-24 19:53:58 -0100

    Channels y persistencia de datso

--- app/Models/CommunityLink.php
+++ app/Models/CommunityLink.php
@@ -10,11 +10,20 @@ class CommunityLink extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id', 'channel_id', 'title', 'link', 'approved'
+        'user_id',
+        'channel_id',
+        'title',
+        'link',
+        'approved'
       ];
 
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class, 'channel_id');
     }
 }
--- resources/views/layouts/add-link.blade.php
+++ resources/views/layouts/add-link.blade.php
@@ -11,6 +11,9 @@
                     {{$link->title}}
                 </a>
                 <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
+                <span class="label label-default" style="background: {{ $link->channel->color }}">
+                    {{ $link->channel->title }}
+                    </span>
             </li>
             @endforeach
 
@@ -26,7 +29,7 @@
                         <div class="form-group">
                             <label for="title">Title:</label>
         
-                            <input type="text" class="form-control @error ('title') is-invalid @enderror" id="title" name="title" placeholder="What is the title of your article?">
+                            <input type="text" class="form-control @error ('title') is-invalid @enderror" id="title" name="title" placeholder="What is the title of your article?" value="{{old('title')}}">
                             @error('title')
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
@@ -34,7 +37,7 @@
         
                         <div class="form-group">
                             <label for="link">Link:</label>
-                            <input type="text" class="form-control @error ('link') is-invalid @enderror" id="link" name="link" placeholder="What is the URL?">
+                            <input type="text" class="form-control @error ('link') is-invalid @enderror" id="link" name="link" placeholder="What is the URL?" value="{{old('link')}}>
                             @error('title')
                                 <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
@@ -42,6 +45,20 @@
         
                         <div class="form-group card-footer">
                             <button class="btn btn-primary">Contribute Link</button>
+                        </div>
+                        <div class="form-group">
+                            <label for="Channel">Channel:</label>
+                                <select class="form-control @error('channel_id') is-invalid @enderror" name="channel_id">
+                                    <option  selected disabled>Pick a Channel...</option>
+                                    @foreach ($channels as $channel)
+                                        <option value = "{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                            {{ $channel->title }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            @error('channel_id')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </form>
                 </div>
