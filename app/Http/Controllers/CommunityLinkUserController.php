2b03f32e24986cb716104a4088e40a05baf051df
helenarodmun <helenarodmun@gmail.com>
2023-02-15 15:40:58 +0000

    fallo desde el equipo personal

--- app/Http/Controllers/CommunityLinkUserController.php
+++ app/Http/Controllers/CommunityLinkUserController.php
@@ -10,22 +10,27 @@ use Illuminate\Support\Facades\Auth;
 class CommunityLinkUserController extends Controller
 {
 
-    public function store(CommunityLink $link)
-    {
-        // Se guarda en la variable el usuario autenticado a través de la id búscandolo
-        // con el método firstOrNew que encuentra el primer registro de la tabla pivote que coincida con las restricciones dadas,
-        // y si no crea uno nuevo, en este caso buscara el usuario que está logueado, si tiene el id del link que se la pasa a través del action del formulario por url
-        //(si lo ha votado)    
-        $vote = CommunityLinkUser::firstOrNew(['user_id' => Auth::id(), 'community_link_id' => $link->id]);
-        //si tiene el id del link, lo borrará al hacer click
-        if ($vote->id) {
-            $vote->delete();
-            //si no lo tiene se guardará en la base de datos el comunnity_link_id que se le pasa por url através del action del formulario
-        } else {
-            $vote->save();
-        }
-        return back();
-    }
+    // public function store(CommunityLink $link)
+    // {
+    //     // Se guarda en la variable el usuario autenticado a través de la id búscandolo
+    //     // con el método firstOrNew que encuentra el primer registro de la tabla pivote que coincida con las restricciones dadas,
+    //     // y si no crea uno nuevo, en este caso buscara el usuario que está logueado, si tiene el id del link que se la pasa a través del action del formulario por url
+    //     //(si lo ha votado)    
+    //     $vote = CommunityLinkUser::firstOrNew(['user_id' => Auth::id(), 'community_link_id' => $link->id]);
+    //     //si tiene el id del link, lo borrará al hacer click
+    //     if ($vote->id) {
+    //         $vote->delete();
+    //         //si no lo tiene se guardará en la base de datos el comunnity_link_id que se le pasa por url através del action del formulario
+    //     } else {
+    //         $vote->save();
+    //     }
+    //     return back();
+    // }
+    public function store(CommunityLink $link, CommunityLinkUser $communityLinkUser)
+{
+    $communityLinkUser->toggleVote($link);
+    return back();
+}
 }
 
 //OJO!! si da errores limpiar la cache de rutas y config php artisan config:clear
--- app/Models/CommunityLinkUser.php
+++ app/Models/CommunityLinkUser.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class CommunityLinkUser extends Model
 {
@@ -15,5 +16,14 @@ class CommunityLinkUser extends Model
     ];
   protected $table = 'community_link_user';
   //forzado del nombre de la tabla por conflicto con las consultas sql -> Illuminate \ Database \ QueryException (42S02) SQLSTATE[42S02]: Base table or view not found: 1146 Table
-
+  public function toggleVote(CommunityLink $link)
+  {
+      $vote = $this->firstOrNew(['user_id' => Auth::id(), 'community_link_id' => $link->id]);
+      if ($vote->id) {
+          $vote->delete();
+      } else {
+          $vote->save();
+      }
+  }
+  
 }
