e931b821286e4464b5e4c1355a2aff3417c8c2cf
helenarodmun <helenarodmun@gmail.com>
2023-02-15 19:20:27 -0100

    Sistema de votación II, terminado última parte

--- app/Models/CommunityLinkUser.php
+++ app/Models/CommunityLinkUser.php
@@ -14,16 +14,16 @@ class CommunityLinkUser extends Model
         'user_id',
         'community_link_id',
     ];
-  protected $table = 'community_link_user';
-  //forzado del nombre de la tabla por conflicto con las consultas sql -> Illuminate \ Database \ QueryException (42S02) SQLSTATE[42S02]: Base table or view not found: 1146 Table
-  public function toggleVote(CommunityLink $link)
-  {
-      $vote = $this->firstOrNew(['user_id' => Auth::id(), 'community_link_id' => $link->id]);
-      if ($vote->id) {
-          $vote->delete();
-      } else {
-          $vote->save();
-      }
-  }
-  
+     //forzado del nombre de la tabla por conflicto con las consultas sql -> Illuminate \ Database \ QueryException (42S02) SQLSTATE[42S02]: Base table or view not found: 1146 Table
+    protected $table = 'community_link_user';
+   
+    public function toggleVote(CommunityLink $link)
+    {
+        $vote = $this->firstOrNew(['user_id' => Auth::id(), 'community_link_id' => $link->id]);
+        if ($vote->id) {
+            $vote->delete();
+        } else {
+            $vote->save();
+        }
+    }
 }
